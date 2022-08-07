c91e1699d1e22314582be8754036dbb0a8903dda
jobaer-ahmed <ajobaer08@gmail.com>
2022-08-07 16:19:32 +0800

    min is array

--- largestInArray.js
+++ largestInArray.js
@@ -9,7 +9,20 @@ function maxInArray(numbers) {
     }
     return largest;
 }
+function minInArray(numbers) {
+    let min = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element < min) {
+            min = element;
+        }
+    }
+    return min;
+}
 
 const heights = [167, 190, 120, 165, 137, 265];
 const tallest = maxInArray(heights);
 console.log("Tallest: ", tallest)
+const smallest = minInArray(heights);
+console.log("Smallest: ", smallest)
